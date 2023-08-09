e9b0c0d8515dcf5d171ffb4f87c4bab5058e3a61
Backas03 <71801733+Backas03@users.noreply.github.com>
2023-08-09 09:10:39 +0900

    Code clean-up

--- src/main/java/kr/backas/nanoore/listener/OreBreakListener.java
+++ src/main/java/kr/backas/nanoore/listener/OreBreakListener.java
@@ -7,11 +7,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 public class OreBreakListener implements Listener {
-
     @EventHandler
     public void onBreak(BlockBreakEvent e) {
         Mine mine = MineManager.getInstance().findMine(e.getBlock());
         e.setCancelled(mine != null && mine.breakBlock(e.getPlayer(), e.getBlock()));
     }
-
 }
